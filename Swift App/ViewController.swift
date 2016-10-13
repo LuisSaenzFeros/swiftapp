f42fcc4e62caee2511b08d0d37a603953c68999c
Luis <luis.saenz6555@gmail.com>
2016-10-13 15:51:16 -0700

    first commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.theLabel.text = "TESSssSST"
     }
 
     override func didReceiveMemoryWarning() {
